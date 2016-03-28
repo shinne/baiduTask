a05e6fb02eed453f6f99d3baeb669fe20a0e30a0
shinne_you <675519530@qq.com>
2016-03-28 19:34:28 +0800

    task1_16未完待续

--- task1_16/task1_16.js
+++ task1_16/task1_16.js
@@ -7,12 +7,32 @@
  * };
  */
 var aqiData = {};
-
+var sCity;
+var sValue;
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
+	var sCity = document.getElementById("aqi-city-input").value;
+	var sValue = document.getElementById("aqi-value-input").value;
+	//匹配中英文字符
+	var reCity = /^\s*[\u4e00-\u9fa5_a-zA-Z]+\s*$/gi;
+	//匹配数字
+	var reValue = /-?\d+/gi;
+	if(!reCity.test(sCity)){
+		alert("城市名输入不正确，请重新输入！");
+		return;
+	}
+	else if(!reValue.test(sValue)){
+		alert("空气质量指数输入不正确，请重新输入！")
+		return;
+	}
+	//将前后的空格去掉
+	var sCity = sCity.replace(/(^\s*) | (\s*$)/g,'');
+	sValue = sValue.replace(/(^\s*) | (\s*$)/g,'');
+	console.log(sCity);
+	console.log(sValue);
 
 }
 
@@ -45,9 +65,15 @@ function delBtnHandle() {
 function init() {
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+  var oBtn = document.getElementById("add-btn");
+  oBtn.onclick = function(){
+      addBtnHandle();
+  }
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }
+window.onload = function(){
+	init();
+}
 
-init();
+
